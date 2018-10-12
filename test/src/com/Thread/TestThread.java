d24b8ef08464996a6717617318d70e7804b93035
对六六 <pengrui55555@163.com>
2018-10-12 22:13:20 +0800

    更新Java线程通信

---
+++ test/src/com/Thread/TestThread.java
@@ -0,0 +1,59 @@
+package com.Thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * TestThread
+ */
+public class TestThread {
+
+    public static void main(String[] args) {
+        int max = 10;
+        Object lock = new Object();
+        Thread A = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.print("A");
+
+            }
+        });
+        Thread B = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                System.out.print("B");
+
+            }
+        });
+        Thread C = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                System.out.print("C");
+
+            }
+        });
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        for (int i = 0; i < max; i++) {
+            executorService.submit(A);
+            executorService.submit(B);
+            executorService.submit(C);
+        }
+
+        executorService.shutdown();
+    }
+
+    private static void printNumber(String threadName) {
+        int i = 0;
+        while (i++ < 10) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(threadName + " print: " + i);
+        }
+    }
+
+}
