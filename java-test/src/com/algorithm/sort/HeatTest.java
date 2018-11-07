ab1c963f02a60d74b18647a4537492557e0b07ea
对六六 <pengrui55555@163.com>
2018-11-07 22:09:57 +0800

    更新堆排序

--- java-test/src/com/algorithm/sort/HeatTest.java
+++ java-test/src/com/algorithm/sort/HeatTest.java
@@ -9,27 +9,22 @@ public class HeatTest {
 
     public static void main(String[] args) {
         int[] nums = { 1, 3, 4, 6, 7, 5, 2, 9 };
-        heapSort(nums);
+        heapsort(nums);
         System.out.println("heapSorted: " + ArrayMethod.StringfyArray(nums));
     }
 
-    public static void heapSort(int[] nums) {
+    public static void heapsort(int[] nums) {
         initHeap(nums);
-        sort(nums);
-    }
-
-    private static void sort(int[] nums) {
-        int len = nums.length;
-        for (int i = len - 1; i >= 0; i--) {
+        for (int i = nums.length - 1; i >= 0; i--) {
             ArrayMethod.swap(nums, i, 0);
             judgeHeap(nums, i, 0);
         }
     }
 
     private static void initHeap(int[] nums) {
-        int i = (nums.length - 1) >> 1;
-        for (; i >= 0; i--) {
-            judgeHeap(nums, nums.length, i);
+        int parentIndex = (nums.length - 1) >> 1;
+        for (; parentIndex >= 0; parentIndex--) {
+            judgeHeap(nums, nums.length, parentIndex);
         }
     }
 
@@ -38,16 +33,15 @@ public class HeatTest {
         int right = (index << 1) + 2;
         int largest = index;
 
-        if (size > left && nums[largest] < nums[left]) {
+        if (left < size && nums[left] > nums[largest])
             largest = left;
-        }
-        if (size > right && nums[largest] < nums[right]) {
+        if (right < size && nums[right] > nums[largest])
             largest = right;
-        }
-        if (index != largest) {
-            ArrayMethod.swap(nums, index, largest);
+        if (largest != index) {
+            ArrayMethod.swap(nums, largest, index);
             judgeHeap(nums, size, largest);
         }
 
     }
+
 }
